6d2e1b5f7fc3aada2a6c010c5e0e9b5889383de5
unknown <pashabaihaqqi72@gmail.com>
2021-04-23 14:21:34 +0700

    First Pick

---
+++ index.js
@@ -0,0 +1,64 @@
+
+function kotak(panjang) {
+    let cetak = '';
+    for (let i = 0; i < panjang; i++) {
+        for (let j = 0; j < panjang; j++) {
+            cetak += '* ';
+        }
+        cetak += '\n';
+    }
+    return cetak;
+}
+console.log(kotak(5));
+
+function segitiga(baris){
+    let cetak ='';
+    for (let i = 0; i <=baris; i++){
+        for(let j = 0; j < i; j++){
+            cetak += '*';
+        }
+        cetak += '\n';
+    }
+    return cetak;
+}
+console.log(segitiga(5))
+
+function segitigaTerbalik(baris) {
+    let cetak ='';
+    for (let i = 0; i <baris; i++){
+        for(let j = i; j <baris; j++){
+            cetak += '*';
+        }
+        cetak += '\n';
+    }
+    return cetak;
+}console.log(segitigaTerbalik(5))
+
+
+function selangSeling(baris,kolom) {
+    let cetak ='';
+    for (let i = 0; i <baris; i++){
+        for(let j = 0; j <kolom; j++){
+            if((i %2==1 || j %2==0)&&(j %2==1 || i %2==0)){
+            cetak += '*';
+        }else {
+            cetak += '!';   
+        }
+        }cetak += '\n';
+    }
+    return cetak;
+}console.log(selangSeling(5,5));
+
+function kotakPola(baris,kolom) {
+    let cetak ='';
+    for (let i = 0; i <baris; i++){
+        for(let j = 0; j <kolom; j++){
+            if((i%2==0 || j!==1) && (i%2==1 || j!==2)) {
+            cetak += '*';
+        }else{
+            cetak += '!';
+        } 
+        }cetak += '\n';
+    }
+    return cetak;
+}console.log(kotakPola(5,5));
